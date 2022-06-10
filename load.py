0ff5671e491cd16e5793628606b947cb74b92dde
ccbonilla <70046987+ccbonilla@users.noreply.github.com>
2022-06-10 18:47:20 -0500

    proyect

---
+++ load.py
@@ -0,0 +1,51 @@
+import os
+import boto3
+import errno
+
+buketRaw='raw-data-bucket-bi'
+s3=boto3.client('s3',aws_access_key_id='AKIAQPZIOPJXLYAQFNNS',aws_secret_access_key="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")
+
+
+def assert_dir_exists(path):
+    """
+    Checks if directory tree in path exists. If not it created them.
+    :param path: the path to check if it exists
+    """
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def download_dir(client, bucket, path, target):
+    """
+    Downloads recursively the given S3 path to the target directory.
+    :param client: S3 client to use.
+    :param bucket: the name of the bucket to download from
+    :param path: The S3 directory to download.
+    :param target: the local directory to download the files to.
+    """
+
+    # Handle missing / at end of prefix
+    if not path.endswith('/'):
+        path += '/'
+
+    paginator = client.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket=bucket, Prefix=path):
+        # Download each file individually
+        for key in result['Contents']:
+            # Calculate relative path
+            rel_path = key['Key'][len(path):]
+            # Skip paths ending in /
+            if not key['Key'].endswith('/'):
+                local_file_path = os.path.join(target, rel_path)
+                # Make sure directories exist
+                local_file_dir = os.path.dirname(local_file_path)
+                assert_dir_exists(local_file_dir)
+                client.download_file(bucket, key['Key'], local_file_path)
+
+
+
+
+download_dir(s3, buketRaw, 'api', 'files')
